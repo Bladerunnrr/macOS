3eea55ddcf3de0e617e92c1264a941c5fb9a1faa
Soulsborne <96502429+Bladerunnrr@users.noreply.github.com>
2021-12-23 18:07:47 +1100

    Update macapps-link.sh

--- macapps-link.sh
+++ macapps-link.sh
@@ -17,7 +17,15 @@ echo $"
 ###############################
 #    Define worker functions  #
 ###############################
-
+versionChecker() {
+	local v1=$1; local v2=$2;
+	while [ `echo $v1 | egrep -c [^0123456789.]` -gt 0 ]; do
+		char=`echo $v1 | sed 's/.*\([^0123456789.]\).*/\1/'`; char_dec=`echo -n "$char" | od -b | head -1 | awk {'print $2'}`; v1=`echo $v1 | sed "s/$char/.$char_dec/g"`; done
+	while [ `echo $v2 | egrep -c [^0123456789.]` -gt 0 ]; do
+		char=`echo $v2 | sed 's/.*\([^0123456789.]\).*/\1/'`; char_dec=`echo -n "$char" | od -b | head -1 | awk {'print $2'}`; v2=`echo $v2 | sed "s/$char/.$char_dec/g"`; done
+	v1=`echo $v1 | sed 's/\.\./.0/g'`; v2=`echo $v2 | sed 's/\.\./.0/g'`;
+	checkVersion "$v1" "$v2"
+}
 
 checkVersion() {
 	[ "$1" == "$2" ] && return 1
@@ -62,6 +70,32 @@ installApp() {
 #    Install selected apps    #
 ###############################
 installApp "dmg" "Chrome" "Google Chrome.app" "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg" "" "" ""
+installApp "dmg" "HandBrake" "HandBrake.app" "https://download.handbrake.fr/releases/1.1.1/HandBrake-1.1.1.dmg" "" "" ""
+installApp "dmg" "Steam" "Steam.app" "http://media.steampowered.com/client/installer/steam.dmg" "" "" "build"
+installApp "dmg" "VLC" "VLC.app" "http://get.videolan.org/vlc/3.0.16/macosx/vlc-3.0.16-intel64.dmg" "" "" ""
+installApp "dmg" "OBS Studio" "OBS Studio.app" "https://cdn-fastly.obsproject.com/downloads/obs-mac-27.1.3.dmg" "" "" ""
+
+# Examples
+# installApp "zip" "Chromium" "Chromium.app" "https://download-chromium.appspot.com/dl/Mac" "chrome-mac/" "" ""
+# installApp "zip" "GitHub" "GitHub Desktop.app" "https://central.github.com/deployments/desktop/desktop/latest/darwin" "" "" ""
+# installApp "tar" "FileZilla" "FileZilla.app" "https://download.filezilla-project.org/client/FileZilla_latest_macosx-x86.app.tar.bz2" "" "" ""
+# installApp "dmg" "Firefox" "Firefox.app" "http://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US" "" "" ""
+# installApp "dmg" "SublimeText" "Sublime Text.app" "https://download.sublimetext.com/sublime_text_build_4126_mac.zip" "" "" ""
+# installApp "zip" "Visual Studio Code" "Visual Studio Code.app" "http://go.microsoft.com/fwlink/?LinkID=620882" "" "" ""
+# installApp "zip" "iTerm2" "iTerm.app" "https://iterm2.com/downloads/stable/latest" "" "" ""
+# installApp "zip" "Cyberduck" "Cyberduck.app" "https://update.cyberduck.io/Cyberduck-4.6.zip" "" "" ""
+# installApp "dmg" "KeePassX" "KeePassX.app" "http://www.keepassx.org../releases/2.0.3/KeePassX-2.0.3.dmg" "" "" ""
+# installApp "dmg" "Etcher" "Etcher.app" "https://github.com/balena-io/etcher/releases/download/v1.5.45/balenaEtcher-1.5.45.dmg" "" "" ""
+# installApp "dmg" "VLC" "VLC.app" "http://get.videolan.org/vlc/3.0.16/macosx/vlc-3.0.16-intel64.dmg" "" "" ""
+# installApp "dmg" "GIMP" "Gimp-2.10.app" "http://download.gimp.org/mirror/pub/gimp/v2.10/osx/gimp-2.10.30-x86_64.dmg" "" "" ""
+# installApp "dmg" "Steam" "Steam.app" "http://media.steampowered.com/client/installer/steam.dmg" "" "" "build"
+# installApp "dmg" "Kodi" "Kodi.app" "http://mirrors.kodi.tv/releases/osx/x86_64/kodi-18.8-Leia-x86_64.dmg" "" "" ""
+# installApp "zip" "Plex" "Plex Media Player.app" "https://downloads.plex.tv/plexmediaplayer/2.58.0.1076-38e019da/PlexMediaPlayer-2.58.0.1076-38e019da-macosx-x86_64.zip" "" "" ""
+# installApp "dmg" "HandBrake" "HandBrake.app" "https://download.handbrake.fr/releases/1.1.1/HandBrake-1.1.1.dmg" "" "" ""
+# installApp "dmg" "Skype" "Skype.app" "http://www.skype.com/go/getskype-macosx.dmg" "" "" ""
+# installApp "dmg" "Slack" "Slack.app" "https://slack.com/ssb/download-osx" "" "" ""
+# installApp "dmg" "WhatsApp" "WhatsApp.app" "https://web.whatsapp.com/desktop/mac/files/WhatsApp.dmg" "" "" ""
+# installApp "dmg" "Discord" "Discord.app" "https://discordapp.com/api/download?platform=osx" "" "" ""
 
 ###############################
 #    Print script footer      #
@@ -71,3 +105,4 @@ echo $'\360\237\222\254  - Thank you for using macapps.link!! Liked it? Recommen
 echo $'\360\237\222\260  - The time is gold. Have I saved you a lot? :) - https://macapps.link/donate'
 echo $'--------------------------------------------------------------------------------\n'
 rm -rf ~/macapps
+
